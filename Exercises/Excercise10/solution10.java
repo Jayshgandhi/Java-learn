2bec5efbbc698ddb4375509bcef74a7ecdf21238
Jayashree S <jayshgandhi95@gmail.com>
2021-02-23 14:11:17 +0900

    solution10 updated with appropriate arguments

--- Exercises/Excercise10/solution10.java
+++ Exercises/Excercise10/solution10.java
@@ -1,16 +1,12 @@
 class Arithmetic{
-    private int n=89;
-    private int y=67;
-    public int add(){
+    public int add(int n,int y){
         return n+y;
     }
-    public int sub(){
+    public int sub(int n,int y){
         return n-y;
     }
-    public int mul(){
-        return n*y;
-    }
-    public int div(){
+    public int mul(int n,int y){ return n*y; }
+    public int div(int n,int y){
         return n/y;
     }
 }
@@ -18,10 +14,10 @@ public class solution10 {
 
     public static void main(String[] args) {
         Arithmetic ar =new Arithmetic();
-        System.out.println("Arithmetic add:"+ar.add());
-        System.out.println("Arithmetic sub:"+ar.sub());
-        System.out.println("Arithmetic multiplication:"+ar.mul());
-        System.out.println("Arithmetic division:"+ar.div());
+        System.out.println("Arithmetic add:"+ar.add(4,7));
+        System.out.println("Arithmetic sub:"+ar.sub(34,5));
+        System.out.println("Arithmetic multiplication:"+ar.mul(23,78));
+        System.out.println("Arithmetic division:"+ar.div(56,12));
 
     }
 }
