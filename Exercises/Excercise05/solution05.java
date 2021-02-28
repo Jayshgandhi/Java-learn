187d39e5a5756290c767c2a2bd5676d95c3a2ba5
Jayashree S <jayshgandhi95@gmail.com>
2021-02-28 19:05:09 +0900

    solution05 completed

---
+++ Exercises/Excercise05/solution05.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class solution05 {
+    public static void main(String[] args) {
+        System.out.println("Enter a integer value:");
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Multiplicative value of given n integers:"+mult(n));
+
+    }
+    public static float mult(int n){
+        float f[] =new float[n];
+        float m=1f;
+        Random rd = new Random();
+        for (int i = 0; i < n; i++) {
+            f[i] = (float)(rd.nextInt((n - 1) + 1) + 1);
+            m = m*f[i];
+        }
+        return m;
+    }
+
+}
+
